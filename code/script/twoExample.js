5da3d45cab64abd92691fb0c0f32655b893fd77c
yangxiaoping <yangxiaoping@ihealthlabs.com.cn>
2017-01-13 10:10:26 +0800

    node.js 实战:如何优雅的处理异常 补充

--- code/script/twoExample.js
+++ code/script/twoExample.js
@@ -68,25 +68,59 @@
 //funcAsync();
 
 /**
+ *  多层try...catch... 如何处理异常
+ */
+//async function funcAsync_1(){
+//    try{
+//        await funcAsync_2();
+//    } catch(e){
+//        console.log('funcAsync_1');
+//        console.log(e);
+//    }
+//    console.log('funcAsync_1 ----  end');
+//}
+//
+//async function funcAsync_2(){
+//    for(let i=0; i<3; i++){
+//        console.log('\n---begin'+ i +'---\n');
+//        try{
+//            await ih_func(i);
+//        } catch(e){
+//            console.log(e);
+//        }
+//        console.log('------end'+ i +'-----');
+//    }
+//}
+//
+//function ih_func(args){
+//    return new Promise(function (resolve, reject) {
+//        //这里相当于throw一个异常了
+//        reject('error' + args);
+//    });
+//}
+//
+//funcAsync_1();
+
+/**
  *  如何提高程序的健壮性
  */
-async function funcAsync(){
-    for(let i=0; i<3; i++){
-        console.log('\n---begin'+ i +'---\n');
-        try{
-            await ih_func(i);
-        } catch(e){
-            console.log(e);
-        }
-        console.log('------end'+ i +'-----');
-    }
-}
-
-function ih_func(args){
-    return new Promise(function (resolve, reject) {
-        //这里相当于throw一个异常了
-        reject('error' + args);
-    });
-}
-
-funcAsync();
+//async function funcAsync(){
+//    for(let i=0; i<3; i++){
+//        console.log('\n---begin'+ i +'---\n');
+//        try{
+//            await ih_func(i);
+//        } catch(e){
+//            console.log(e);
+//        }
+//        console.log('------end'+ i +'-----');
+//    }
+//}
+//
+//function ih_func(args){
+//    return new Promise(function (resolve, reject) {
+//        //这里相当于throw一个异常了
+//        reject('error' + args);
+//    });
+//}
+//
+//funcAsync();
